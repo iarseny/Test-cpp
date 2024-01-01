a9abf8a0751bc08dc70b3e0d9768967f104224ed
iarseny <122807698+iarseny@users.noreply.github.com>
2024-01-01 18:02:00 +0000

    commit

---
+++ compile_test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    
+    cout << sizeof(int *) << endl;
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+using namespace std;
+
+int a = 5;
+
+int main() {
+    cout << &a << endl;
+
+    int b = 8;
+    cout << &b;
+
+    return 0;
+}
